2594350cdd5a5faaeaeef5aab28b2b914509f433
황혜빈 <hebin99@naver.com>
2021-03-28 16:18:11 +0900

    블루투스 연결 상태 확인 및 연결 코드 추가
    앱이 실행되면 블루투스 연결 상태를 확인하고 연결이 되어있지 않으면 연결하도록 구현.

--- app/src/main/java/com/example/refresh_selection/MainActivity.kt
+++ app/src/main/java/com/example/refresh_selection/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.refresh_selection
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val BluetoothAdapter.isDisabled: Boolean
+        get() = !isEnabled
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bluetoothAdapter?.takeIf { it.isDisabled }?.apply {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, 1)
+        }
     }
+
+
+
+
+
+
 }
