2eae3156bb94ce9092cf7fe818b8e4b0e0fe0657
hyorinkim <gyfls7748@gmail.com>
2021-09-13 20:24:34 +0900

    그래프 자바 클래스 구현 및 설문조사 자바 수정

--- app/src/main/java/com/example/refresh_selection/Survey.java
+++ app/src/main/java/com/example/refresh_selection/Survey.java
@@ -14,7 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class Survey extends AppCompatActivity {
 
-    static final String[] List_menu={"4:00-6:00","6:00-8:00","8:00-10:00","10:00-12:00","12:00-14:00","14:00-16:00","16:00-18:00","18:00-20:00","20:00-22:00","22:00-00:00"};
+    static final String[] List_menu={"6:00-8:00","8:00-10:00","10:00-12:00","12:00-14:00","14:00-16:00","16:00-18:00","18:00-20:00","20:00-22:00","22:00-00:00"};
 //활동시간대 선택할 아이템들
 
 //장소 카테고리
@@ -24,55 +24,55 @@ public class Survey extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState) ;
-        setContentView(R.layout.survey) ;
+        super.onCreate(savedInstanceState);
+//        super.onCreate(savedInstanceState);
+        setContentView(R.layout.survey);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, List_menu) ;
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, List_menu);
 
-        ListView listview = (ListView) findViewById(R.id.survey_act_time) ;
-        listview.setAdapter(adapter) ;
+        ListView listview = (ListView) findViewById(R.id.survey_act_time);
+
+        listview.setAdapter(adapter);
         //check 박스로 바꾸자...
 //        싹다 바꾸자
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             // 코드 계속 ...
             @Override
             public void onItemClick(AdapterView parent, View v, int position, long id) {
-
                 // get TextView's Text.
-
-                act_time = (String) parent.getItemAtPosition(position) ;
-                Log.d("선택한 활동시간대",act_time);
+                act_time = (String) parent.getItemAtPosition(position);
+                Log.d("선택한 활동시간대", act_time);
                 //배열일까 그냥 하나의 스트링
                 //volley로 데이터베이스에 보냅니다.
                 // TODO : use strText
             }
-        }) ;
+        });
 
-        next= findViewById(R.id.survey_next);
+        next = findViewById(R.id.survey_next);
 
-        next.setOnClickListener(new View.OnClickListener(){
+        next.setOnClickListener(new View.OnClickListener() {
             Intent login_survey = getIntent();
-            String UserId= login_survey.getStringExtra("UserId");
+            String UserId = login_survey.getStringExtra("UserId");
+
             @Override
             public void onClick(View view) {
-                if(act_time==null){
+                if (act_time == null) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(Survey.this);
                     dialog = builder.setMessage("활동 시간대를 선택해 주세요.").setNegativeButton("확인", null).create();
                     dialog.show();
                     return;
 
                 }
-                Intent intent = new Intent(Survey.this,Survey_place.class);
-                intent.putExtra("act_time",act_time);
-                Log.d("survey_act_time",act_time);
-                intent.putExtra("UserId",UserId);
-                Log.d("survey_userId",UserId);
-                Log.d("다음 클릭시","활동시간대 선택 완료");
+                Intent intent = new Intent(Survey.this, Survey_place.class);
+                intent.putExtra("act_time", act_time);
+                Log.d("survey_act_time", act_time);
+                intent.putExtra("UserId", UserId);
+                Log.d("survey_userId", UserId);
+                Log.d("다음 클릭시", "활동시간대 선택 완료");
                 startActivity(intent);
 
             }
         });
-
 
 
     }
