5260f9818f9fb3ed3cf99a0d966564c9ad2b110d
hyorinkim <gyfls7748@gmail.com>
2021-10-07 22:25:45 +0900

    마이 페이지 화면 추가 예정

---
+++ app/src/main/java/com/example/refresh_selection/FragmentProfile.java
@@ -0,0 +1,17 @@
+package com.example.refresh_selection;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentProfile  extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_profile, container, false);
+        return v;
+    }
+}
