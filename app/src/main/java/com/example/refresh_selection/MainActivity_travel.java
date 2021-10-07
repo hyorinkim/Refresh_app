dcb9825baff2bc5a4441abd6d93a6cc25796bae1
hyorinkim <gyfls7748@gmail.com>
2021-10-07 22:25:18 +0900

    activity_trip_main 화면 길이 조정
    바텀네비게이션, 툴바 위치 조정 가운데 화면 길이 조정

--- app/src/main/java/com/example/refresh_selection/MainActivity_travel.java
+++ app/src/main/java/com/example/refresh_selection/MainActivity_travel.java
@@ -1,24 +1,79 @@
 package com.example.refresh_selection;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity_travel extends AppCompatActivity implements NavigationHost {
+
+    private BottomNavigationView bottomNavigationView; // 바텀네비게이션 뷰
+    private FragmentManager manager;
+    private FragmentTransaction transaction;
+    private FragmentMain fragmentMain;//프레임 레이아웃을 액티미티 메인 xml
+    private FragmentMap fragmentMap;//프레임 레이아웃을 액티비티 맵 xml
+    private FragmentProfile fragmentProfile;//프레임 레이아웃을 액티비티 프로파일 xml 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_main);
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.container, new ProductGridFragment())
-                    .commit();
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.trip_navigationView);//바텀 네비게이션
+        //바텀네비게이션 선택한 아이템이 있을때
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+
+                    case R.id.HomeItem:
+                        setFrag(0);
+                        break;
+                    case R.id.MapItem:
+                        setFrag(1);
+                        break;
+                    case R.id.MyPageItem:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        fragmentMain = new FragmentMain();
+        fragmentMap = new FragmentMap();
+        fragmentProfile = new FragmentProfile();
+
+        setFrag(0); // 첫화면 설정
+    }
+    // 프래그먼트 교체가 일어나는 메서드
+    private void setFrag(int n){
+
+        manager = getSupportFragmentManager();
+        transaction = manager.beginTransaction();
+
+        switch (n){
+            case 0:
+                transaction.replace(R.id.frameLayout, fragmentMain);
+                transaction.commit();
+                break;
+            case 1:
+                transaction.replace(R.id.frameLayout, fragmentMap);
+                transaction.commit();
+                break;
+            case 2:
+                transaction.replace(R.id.frameLayout, fragmentProfile);
+                transaction.commit();
+                break;
         }
     }
+
 
     /**
      * Navigate to the given fragment.
