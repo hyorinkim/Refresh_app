8001c43d51585f85f6c50da4211dc7614ab8b0e0
hyorinkim <gyfls7748@gmail.com>
2021-10-11 22:29:30 +0900

    string 추가

--- app/src/main/java/com/example/refresh_selection/FragmentMain.java
+++ app/src/main/java/com/example/refresh_selection/FragmentMain.java
@@ -5,13 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class FragmentMain extends Fragment {
     SpaceCardAdapter spaceCardAdapter;
