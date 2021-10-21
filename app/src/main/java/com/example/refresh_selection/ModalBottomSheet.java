996a26e30ca25420bc7da40b7fb65db4bf26c6f0
hyorinkim <gyfls7748@gmail.com>
2021-10-21 22:21:45 +0900

    modalbottom화면에서 카드가생성되는 함수 추가

--- app/src/main/java/com/example/refresh_selection/ModalBottomSheet.java
+++ app/src/main/java/com/example/refresh_selection/ModalBottomSheet.java
@@ -5,15 +5,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ModalBottomSheet extends BottomSheetDialogFragment {
     String TAG = "bottomSheet";
     private BottomSheetListener mListener;
     private View view;
+    //바텀 시트 안에 카드뷰
+    BottomCardAdapter bottomCardAdapter;
+    private RecyclerView bottomRV;
+    private ArrayList<BottomCard> bottomCardArrayList;//카드 정보 저장
 
     public ModalBottomSheet(){//empty constructor
 
@@ -27,7 +35,24 @@ public class ModalBottomSheet extends BottomSheetDialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        mListener = (BottomSheetListener) getContext();
         view = inflater.inflate(R.layout.bottom_sheet, container, false);
+        makeMapSpaceCard(view);
         return view;
+    }
+
+    //장소 카드 만듣
+    public void makeMapSpaceCard(View mainview) {
+        bottomRV = (RecyclerView) mainview.findViewById(R.id.bottom_sheet_recycleview);//activitiy_map : bottomsheet layout
+        bottomRV.setHasFixedSize(true);
+        bottomCardArrayList= new ArrayList<>();
+        bottomCardArrayList.add(new BottomCard("대청호1","호수가 아름답다.","300m","1000원",R.drawable.test_img));
+        bottomCardArrayList.add(new BottomCard("대청호1","호수가 아름답다.","300m","1000원",R.drawable.test_img));
+        bottomCardArrayList.add(new BottomCard("대청호1","호수가 아름답다.","300m","1000원",R.drawable.test_img));
+//        Context con = mainview.getChildAt(1).getContext();
+//        Log.d("context",con+"");
+        bottomCardAdapter =new BottomCardAdapter(mainview.getContext(),bottomCardArrayList);//content가 문제인거 같은데..
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(mainview.getContext(), LinearLayoutManager.VERTICAL, false);
+        bottomRV.setLayoutManager(linearLayoutManager);
+        bottomRV.setAdapter(bottomCardAdapter);
     }
 
     public interface BottomSheetListener {
