6f3f092157d4ad0a6b58410b7e143183a1a596d7
hyorinkim <gyfls7748@gmail.com>
2021-10-26 21:18:10 +0900

    dateRangePicker로 바꿈 버튼 클릭시능 구현 수정

--- app/src/main/java/com/example/refresh_selection/Calendar_java.java
+++ app/src/main/java/com/example/refresh_selection/Calendar_java.java
@@ -1,40 +1,85 @@
 package com.example.refresh_selection;
 
-import android.app.DatePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-import java.util.Calendar;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
 
 public class Calendar_java extends AppCompatActivity {
+    DatePicker cal_picker;
+    ImageButton schedule_add;
+    ImageView select_date;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_kotlin);
 
-        Calendar c = Calendar.getInstance();
-        int mYear = c.get(Calendar.YEAR);
-        int mMonth = c.get(Calendar.MONTH);
-        int mDay = c.get(Calendar.DAY_OF_MONTH);
+        MaterialDatePicker.Builder<Pair<Long,Long>> materialDateBuilder = MaterialDatePicker.Builder.dateRangePicker();
+        materialDateBuilder.setTitleText("SELECT A DATE");
+        Pair p= Pair.create(MaterialDatePicker.thisMonthInUtcMilliseconds(),
+                MaterialDatePicker.todayInUtcMilliseconds());
+        materialDateBuilder.setSelection(p);
+        materialDateBuilder.setTheme(R.style.Theme_App);
+//
 
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+        final MaterialDatePicker materialDatePicker = materialDateBuilder.build();
+        select_date= findViewById(R.id.select_date);
+        select_date.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                //date.setText(dayOfMonth+"/" + (month+1) + "/" + year);
-                Log.d("date",dayOfMonth+"/"+(month+1)+"/"+year);
+            public void onClick(View v) {
+                materialDatePicker.show(getSupportFragmentManager(),"MATERIAL_DATE_PICKER");
             }
-        }, mYear, mMonth, mDay);
+        });
+
+//        cal_picker=findViewById(R.id.cal_picker);//달력 picker 가져옴
+        schedule_add=findViewById(R.id.addPlaceToSchedule);//스케쥴 추가 버튼 가져옴
+        schedule_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                //확인 버튼 눌렀을때?
+            }
+        });
+//
+//        cal_picker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+//            @Override
+//            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+//
+//            }
+//        });
+
+//        Calendar c = Calendar.getInstance();
+//        int mYear = c.get(Calendar.YEAR);
+//        int mMonth = c.get(Calendar.MONTH);
+//        int mDay = c.get(Calendar.DAY_OF_MONTH);
+//
+//        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                //date.setText(dayOfMonth+"/" + (month+1) + "/" + year);
+//                Log.d("date",dayOfMonth+"/"+(month+1)+"/"+year);
+//            }
+//        }, mYear, mMonth, mDay);
 //        datePickerDialog.show();
 
 
-//        MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.dateRangePicker();
-//        materialDateBuilder.setTitleText("SELECT A DATE");
-//
-//        final MaterialDatePicker materialDatePicker = materialDateBuilder.build();
-//        materialDatePicker.show(getSupportFragmentManager(),"MATERIAL_DATE_PICKER");
+
+
 
         //datePicker에 해당하는 프래그먼트 있어야한다.
 //        DatePickerFragment DPF= new DatePickerFragment();
