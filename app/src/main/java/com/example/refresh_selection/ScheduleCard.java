cfe7d7f3a00fbc1a3d7f7ba532e014b994cb4de6
hyorinkim <gyfls7748@gmail.com>
2021-10-31 21:55:25 +0900

    캘린더 스케쥴 클래스

---
+++ app/src/main/java/com/example/refresh_selection/ScheduleCard.java
@@ -0,0 +1,29 @@
+package com.example.refresh_selection;
+
+import android.view.View;
+
+public class ScheduleCard {//
+    private String schedule_date;
+    private View schedule;
+
+    public ScheduleCard(String schedule_date,View schedule){
+        this.schedule_date=schedule_date;
+        this.schedule=schedule;
+    }
+
+    public String getSchedule_date() {
+        return schedule_date;
+    }
+
+    public void setSchedule_date(String schedule_date) {
+        this.schedule_date = schedule_date;
+    }
+
+    public View getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(View schedule) {
+        this.schedule = schedule;
+    }
+}
