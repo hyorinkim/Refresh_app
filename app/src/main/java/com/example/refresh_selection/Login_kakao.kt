5747789312058831b4a885c85d52cc054cd5fd50
hyorinkim <gyfls7748@gmail.com>
2021-11-16 14:29:48 +0900

    사용자 나이와 성별 저장

--- app/src/main/java/com/example/refresh_selection/Login_kakao.kt
+++ app/src/main/java/com/example/refresh_selection/Login_kakao.kt
@@ -8,37 +8,37 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageButton
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.model.KakaoSdkError
 import com.kakao.sdk.user.UserApiClient
 import org.json.JSONObject
 import java.io.BufferedWriter
 import java.io.File
-import java.io.FileOutputStream
 import java.io.FileWriter
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class Login_kakao : AppCompatActivity() {
     @SuppressLint("WrongViewCast")
-    val testUrl = "http://3.19.79.3:8089//api/user/login"
+    val testUrl = "http:/ 3.19.218.150:8089//api/user/login"
     val fileName = "/data/data/com.example.refresh_selection/files"
-    fun csvFirst(g_a:String) {
-        val headerData = "gender,age"
+    fun csvFirst(g_a: String) {
+        val headerData = "Sex,Age,Month,time,day"
         val file = File(fileName, "gender_age.csv")
 
         if (file.exists() == true) {//파일이 있을시
+            Log.d("Loginkakao", "파일 있었네")
             val bw = BufferedWriter(FileWriter(file, true))
             try {
-                bw.write(g_a+"\n")
+                bw.write(g_a + "\n")
                 bw.flush()
                 bw.close()
-                Log.d("fileCreate","파일있을때 append")
+                Log.d("fileCreate", "파일있을때 append")
             } catch (e: Exception) {
                 Toast.makeText(this, "Error on writing file ${e.message}", Toast.LENGTH_LONG).show()
             }
@@ -47,9 +47,8 @@ class Login_kakao : AppCompatActivity() {
             val bw = BufferedWriter(FileWriter(file))
             try {
                 bw.write("$headerData\n")
-                Log.d("fileCreate","파일 없어서 생성 create")
-                bw.newLine()
-                bw.write(g_a+"\n")
+                Log.d("fileCreate", "파일 없어서 생성 create")
+                bw.write(g_a + "\n")
                 bw.flush()
                 bw.close()
             } catch (e: Exception) {
@@ -92,23 +91,42 @@ class Login_kakao : AppCompatActivity() {
                     if (error != null) {
                         Log.e("user info fail", "사용자 정보 요청 실패", error)
                     } else if (user != null) {
-                        if (user.kakaoAccount?.gender != null) {
-                            gender_age += "M,"
-                        } else {
+                        if (user.kakaoAccount?.gender.toString().equals("FEMALE")) {
                             gender_age += "F,"
+                        } else {
+                            gender_age += "M,"
                         }
-                        if (user.kakaoAccount?.ageRange != null) {
-                            gender_age += "20,"
-                        }
+                        var age=user.kakaoAccount?.ageRange.toString().split("_")
+                        var rdn = (0..10).random()
+                     when(age[1]){
+                         "10" -> gender_age += "" + (10 + rdn)+","
+                         "20" -> gender_age += "" + (20 + rdn)+","
+                         "30" -> gender_age += "" + (30 + rdn)+","
+                         "40" -> gender_age += "" + (40 + rdn)+","
+                         "50" -> gender_age += "" + (50 + rdn)+","
+                         "60" -> gender_age += "" + (60 + rdn)+","
+                         "70" -> gender_age += "" + (70 + rdn)+","
+                     }
+                        val dt = Date()
+                        val month = (dt.month + 1).toString() + "" //month+1 한 값이 월
+                        val hour = dt.hours.toString() + ""
+                        val sdf = SimpleDateFormat("EE") //요일
+                        val day = sdf.format(dt).toString() //요일
+
+                        gender_age += "$month,$hour,$day"
+//                    SimpleDateFormat full_sdf = new SimpleDateFormat("yyyy-MM-dd, hh:mm:ss a");
+
                         csvFirst(gender_age)
 
                         nextIntent.putExtra("gender_age", gender_age)
-                        Log.i("user info success", "사용자 정보 요청 성공" +
-                                "\n회원번호: ${user.id}" +
-                                "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
-                                "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}" +
-                                "\n성별: ${user.kakaoAccount?.gender}" +
-                                "\n나이: ${user.kakaoAccount?.ageRange}" + "\n성별나이" + gender_age)
+                        Log.i(
+                            "user info success", "사용자 정보 요청 성공" +
+                                    "\n회원번호: ${user.id}" +
+                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}" +
+                                    "\n성별: ${user.kakaoAccount?.gender}" +
+                                    "\n나이: ${user.kakaoAccount?.ageRange}" + "\n성별나이" + gender_age
+                        )
 
                         val myJson = JSONObject()
                         myJson.put("UserNumber", user.id)//회원번호
@@ -119,11 +137,15 @@ class Login_kakao : AppCompatActivity() {
                         val requestBody = myJson.toString()
                         /* myJson에 아무 데이터도 put 하지 않았기 때문에 requestBody는 "{}" 이다 */
 
-                        val testRequest = object : StringRequest(Method.POST, testUrl, Response.Listener { response ->
-                            println("서버 Response 수신: $response")//가져오기 성공
-                        }, Response.ErrorListener { error ->
-                            Log.d("ERROR", "서버 Response 가져오기 실패: $error")
-                        }) {
+                        val testRequest = object : StringRequest(
+                            Method.POST,
+                            testUrl,
+                            Response.Listener { response ->
+                                println("서버 Response 수신: $response")//가져오기 성공
+                            },
+                            Response.ErrorListener { error ->
+                                Log.d("ERROR", "서버 Response 가져오기 실패: $error")
+                            }) {
                             override fun getBodyContentType(): String {
                                 return "application/json; charset=utf-8"
                             }
@@ -171,13 +193,17 @@ class Login_kakao : AppCompatActivity() {
         val requestBody = myJson.toString()
         /* myJson에 아무 데이터도 put 하지 않았기 때문에 requestBody는 "{}" 이다 */
 
-        val testRequest = object : StringRequest(Method.POST, testUrl, Response.Listener { response ->
-            println("서버 Response 수신: $response")
-            success(true)
-        }, Response.ErrorListener { error ->
-            Log.d("ERROR", "서버 Response 가져오기 실패: $error")
-            success(false)
-        }) {
+        val testRequest = object : StringRequest(
+            Method.POST,
+            testUrl,
+            Response.Listener { response ->
+                println("서버 Response 수신: $response")
+                success(true)
+            },
+            Response.ErrorListener { error ->
+                Log.d("ERROR", "서버 Response 가져오기 실패: $error")
+                success(false)
+            }) {
             override fun getBodyContentType(): String {
                 return "application/json; charset=utf-8"
             }
